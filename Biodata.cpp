9c84357bc633a0d3dde3d8f7424f46a87489901e
Wahyu Gultom <Wahyu Gultom>
2020-09-16 12:08:39 +0700

    Menambahkan commit file

---
+++ Biodata.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+	char biodata[50];
+	
+	printf("Nama: Wahyu Nugroho Gultom");
+	printf("\n");
+	printf("NPM: 19081010014");
+	printf("\n");
+	printf("TTL: Bengkulu, 29 Desember 2000");
+	printf("\n");
+	printf("Jenis kelamin: Laki-laki");
+	printf("\n");
+	printf("Asal: Bengkulu");
+	printf("\n");
+	printf("Pendidikan saat ini: Mahasiswa");
+	printf("\n");
+	printf("Pendidikan terakhir: SMA N 4 Bengkulu");
+	printf("\n");
+	printf("Hobi: Musik");
+	
+	return 0;
+}
+
+
